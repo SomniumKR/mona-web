f3f6aa5947ff0d66ba2db87e8818ff9700d59f0e
JSKeum <jskeum2512@gmail.com>
2021-05-21 00:28:38 +0900

    add normalize.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import { css, Global } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionNormalize}
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              background: white;
+              min-height: 100%;
+              font-family: sans-serif;
+            }
+        `}
+      />
+      <Component {...pageProps} />
+      ;
+
+    </>
+  );
 }
 
 export default MyApp;
