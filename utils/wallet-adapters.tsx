dd1165bf9b9d30303318614262936a332de6d2dc
JSKeum <jskeum2512@gmail.com>
2021-05-26 22:15:56 +0900

    add wallet adapter (sollet extension)

---
+++ utils/wallet-adapters.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import Wallet from '@project-serum/sol-wallet-adapter';
+import { notify } from 'utils/notification';
+import type { PublicKey, Transaction } from '@solana/web3.js';
+
+export function SolletExtensionAdapter(_, network) {
+  const { sollet } = window as any;
+  if (sollet) {
+    return new Wallet(sollet, network);
+  }
+
+  return {
+    on: () => {},
+    connect: () => {
+      notify({
+        message: 'Sollet Extension Error',
+        description: 'Please install the Sollet Extension for Chrome',
+      });
+    },
+  };
+}
+
+export interface WalletAdapter {
+  publicKey: PublicKey;
+  autoApprove: boolean;
+  connected: boolean;
+  signTransaction: (transaction: Transaction) => Promise<Transaction>;
+  signAllTransactions: (transaction: Transaction[]) => Promise<Transaction[]>;
+  connect: () => any;
+  disconnect: () => any;
+  on<T>(event: string, fn: () => void): this;
+}
