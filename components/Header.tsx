c65ecb415220d585a00ea9c0221d8d57184606b9
JSKeum <jskeum2512@gmail.com>
2021-06-02 18:30:13 +0900

    add Header

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Header = styled.header`
+  width: 100%;
+  height: 62px;
+  display: flex;
+  justify-content: space-between;
+  padding: 7px 15px;
+  box-sizing: border-box;
+`;
+
+export default Header;
