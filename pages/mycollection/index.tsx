2c5fbf09ef322bc870c17ca196f50d407ca98b4e
JSKeum <jskeum2512@gmail.com>
2021-06-08 13:58:37 +0900

    test

--- pages/mycollection/index.tsx
+++ pages/mycollection/index.tsx
@@ -95,14 +95,12 @@ export default function NFT({ nft }:
 
   const getSavedNFT = () => {
     if (wallet && walletConnected) {
-      // try {
-      //   const NFTInfoString = localStorage.getItem(localstorageNameForNFT);
+      const NFTInfoString = localStorage.getItem(localstorageNameForNFT);
 
-      //   const NFTInfo = JSON.parse(NFTInfoString);
-      //   setSavedNFTInfo([NFTInfo]);
-      // } catch (e) {
-      //   console.error(e);
-      // }
+      if (NFTInfoString) {
+        const NFTInfo = JSON.parse(NFTInfoString);
+        setSavedNFTInfo([NFTInfo]);
+      }
     }
   };
 
